04a0baecbfb27b0eb51ebb5a40b206c87e750af8
leolivrare <l220120@dac.unicamp.br>
2020-06-23 15:51:37 -0300

    Service layer, component registration

---
+++ src/main/java/com/llm/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.llm.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.llm.course.entities.User;
+import com.llm.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
